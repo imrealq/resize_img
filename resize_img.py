55aaa694dcd3361b4aa3cd21e4807c868409dece
imrealq <quynhtn29@gmail.com>
2021-03-31 07:49:30 +0700

    list all imgs name in source folder

--- resize_img.py
+++ resize_img.py
@@ -6,9 +6,9 @@ root_folder = os.getcwd()
 # open folder contain file image if not had create one
 source_folder_path = os.path.join(root_folder, "source_imgs")
 os.chdir(source_folder_path)
-# collect all image name in folder
 
-
+# collect all images name
+imgs_name =  os.listdir(source_folder_path)
 
 # resize image
 img_name = "3.jpg"
@@ -19,5 +19,7 @@ resized_img = not_resized_img.resize((98, 152))
 # save image in result folder if not had create one
 result_folder_path = os.path.join(root_folder, "result_imgs")
 os.chdir(result_folder_path)
+
+## check file if exist, name have extend number
 
 resized_img.save("resized_" + img_name)
